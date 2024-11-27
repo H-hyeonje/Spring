18ba301e6c71654d496ac0ef8d907c70b0f1077a
H-hyeonje <guswpwkf@naver.com>
2024-11-27 18:02:33 +0900

    데이터베이스2

--- Spring_bookmarket/src/main/java/com/springmvc/controller/BookController.java
+++ Spring_bookmarket/src/main/java/com/springmvc/controller/BookController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
